070db23c331a4c093176de52eccaa3e573b095ad
rizal <rizalahmaddafa24@gmail.com>
2021-04-26 12:08:48 +0700

    first commit

---
+++ mainpage.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Mulish:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Selamat Datang Di Perpustakaan Online</title>
+    <style>
+        body {
+            overflow-x: hidden;
+            background-color: #F1FEFF;
+            font-family: 'Mulish', sans-serif;
+        }
+        h1 {
+            color: #3E3E3E;
+            font-weight: 800;
+        }
+        p {
+            color: #75787E;
+            padding-top: 2%;
+        }
+        </style>
+</head>
+
+<body>
+    <div class="container" style="margin-top: 15%">
+        <div class="row">
+            <div class="col-md-12 text-center">
+                <h1>Jadikanlah Membaca</h1>
+                <h1>Sebagai Hobimu</h1>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-12 text-center">
+                <p>Buku adalah jendela dunia kita bisa melihat isi dunia tanpa melakukan perjalanan,<br>
+                hanya cukup membaca sebuah halaman</p>
+            </div>
+        </div>
+        <div class="row" style="margin-top: 2%">
+            <div class="col-md-12 text-center">
+            <a type="button" class="btn btn-success" href="http://localhost/form_buku/index.php">Jelajahi Sekarang</a>
+            </div>
+        </div>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
+</body>
+
+</html>
